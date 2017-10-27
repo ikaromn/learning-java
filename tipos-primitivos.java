8bd3e6c4bd2339174a1183be704c6319b1e4d727
Ikaro Lopes <ikaropinheiro6@gmail.com>
2017-10-27 17:00:53 -0200

    Aprendendo sobre os tipos primitivos do Java

---
+++ tipos-primitivos.java
@@ -0,0 +1,19 @@
+//Tipos primitivos
+
+//Tipo inteiro
+int idade = 3;
+float sal = 1925.54f;
+char letra = 'G';
+boolean casado = false;
+
+//Type Cast
+int idade = (int) 3;
+float sal = (float) 1925.54;
+char letra = (char) 'G';
+boolean casado = (boolean) false; 
+
+//by class (Wrapper Class)
+Integer idade = new Integer(3);
+Float sal = new Float(1925.54);
+Character letra = new Character('G');
+Boolean casado = new Boolean(false);
