3a123f398847bc91adf70b99d1b34eadcedb648a
Ikaro Lopes <ikaropinheiro6@gmail.com>
2017-10-27 18:00:12 -0200

    Utulizando operadores aritméticos

--- operadores-aritmeticos.java
+++ operadores-aritmeticos.java
@@ -1,6 +1,10 @@
 public class OperadoresAritmeticos{
 
     public static void main(String[] args){
+        int n1 = 2;
+        int n2 = 3;
 
+        float m = (n1 + n2) / 2;
+        System.out.println("A média é igual a " + m);
     }
 }
